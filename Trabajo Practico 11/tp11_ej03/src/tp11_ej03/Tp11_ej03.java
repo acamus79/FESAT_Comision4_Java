4f9255c0f61d18428d627f31b7773d4f9922f604
acamus79 <adriancamus1979@gmail.com>
2021-07-27 15:00:41 -0300

    Trabajo Practico 11 Ejercicio 02

---
+++ Trabajo Practico 11/tp11_ej03/src/tp11_ej03/Tp11_ej03.java
@@ -0,0 +1,51 @@
+/*
+ * Licencia Libre.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Se plantea desarrollar un programa Java que permita la gestión de una 
+empresa de agroalimentos la cual trabaja con tres tipos de productos: 
+
+productos frescos, productos refrigerados y productos congelados.
+
+Todos los productos llevan información en común: fecha de caducidad y número 
+de lote. A su vez, cada tipo de producto lleva alguna información específica.
+
+Los productos frescos deben llevar la fecha de envasado y el país de origen.
+
+Los productos refrigerados deben llevar el código del organismo de supervisión
+alimentaria, la fecha de envasado, la temperatura de mantenimiento 
+recomendada y el país de origen.
+
+Los productos congelados deben llevar la fecha de envasado, el
+país de origen y la temperatura de mantenimiento recomendada.
+
+Crear el código de las clases Java implementando una relación de herencia 
+siguiendo estas indicaciones:
+a) En primer lugar realizar un esquema con lápiz y papel donde se
+represente cómo se van a organizar las clases cuando escribamos
+el código. Estudiar los atributos de las clases y trasladar a la
+superclase todo atributo que pueda ser trasladado.
+Crear una clase con el método main donde se creen: dos productos
+frescos, tres productos refrigerados y cinco productos congelados .
+Mostrar la información de cada producto por pantalla.
+
+
+ */
+
+package tp11_ej03;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Tp11_ej03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Aqui va todo el código
+    }
+
+}
---
+++ Trabajo Practico 11/tp11_ej03/src/tp11_ej03/entidades/Fresco.java
@@ -0,0 +1,15 @@
+/*
+ * Licencia Libre.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package tp11_ej03.entidades;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class Fresco extends Producto{
+
+}
