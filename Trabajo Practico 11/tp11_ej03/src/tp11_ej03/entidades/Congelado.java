fc64435895a13e7bd1b3909f2b31cdf9b8a91213
acamus79 <adriancamus1979@gmail.com>
2021-07-27 15:49:50 -0300

    Trabajo Practico 11 Ejercicio 03

--- Trabajo Practico 11/tp11_ej03/src/tp11_ej03/entidades/Congelado.java
+++ Trabajo Practico 11/tp11_ej03/src/tp11_ej03/entidades/Congelado.java
@@ -2,14 +2,86 @@
  * Licencia Libre.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
- */
 
+Los productos congelados deben llevar la fecha de envasado, el
+país de origen y la temperatura de mantenimiento recomendada.
+
+ */
 package tp11_ej03.entidades;
 
+import java.time.LocalDate;
+
 /**
- * 
+ *
  * @author Adrian E. Camus
  */
-public class Congelado extends Producto{
+public class Congelado extends Producto {
+
+    private LocalDate fechaEnvasado;
+    private String paisOrigen;
+    private int tempMante;
+
+    public Congelado() {
+    }
+
+    public Congelado(LocalDate fechaEnvasado, String paisOrigen, int tempMante, LocalDate date, long nroLote) {
+        super(date, nroLote);
+        this.fechaEnvasado = fechaEnvasado;
+        this.paisOrigen = paisOrigen;
+        this.tempMante = tempMante;
+    }
+
+    public LocalDate getFechaEnvasado() {
+        return fechaEnvasado;
+    }
+
+    public void setFechaEnvasado(LocalDate fechaEnvasado) {
+        this.fechaEnvasado = fechaEnvasado;
+    }
+
+    public String getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public void setPaisOrigen(String paisOrigen) {
+        this.paisOrigen = paisOrigen;
+    }
+
+    public int getTempMante() {
+        return tempMante;
+    }
+
+    public void setTempMante(int tempMante) {
+        this.tempMante = tempMante;
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public long getNroLote() {
+        return nroLote;
+    }
+
+    @Override
+    public void setNroLote(long nroLote) {
+        this.nroLote = nroLote;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto Congelado\n"
+                + super.toString()
+                + "\nFecha de Envasado: " + fechaEnvasado
+                + "\nPais de Origen: " + paisOrigen
+                + "\nTemperatura de Mantenimiento: " + tempMante + " ° C";
+    }
 
 }
