50fdef12af0012cc652e945f0700bffc588b9ccf
acamus79 <adriancamus1979@gmail.com>
2021-07-28 21:23:29 -0300

    Trabajo Practico 11 Ejercicio 03

--- Trabajo Practico 11/tp11_ej04/src/tp11_ej04/Tp11_ej04.java
+++ Trabajo Practico 11/tp11_ej04/src/tp11_ej04/Tp11_ej04.java
@@ -2,6 +2,38 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+La empresa de desarrollos informáticos “Xtech” necesita llevar
+un registro de todos sus empleados que se encuentran en la
+oficina central, para eso ha creado un diagrama de clases que
+debe incluir lo siguiente:
+1) Empleado
+Atributos:
+- nombre: tipo cadena (Debe ser nombre y apellido)
+- cedula: tipo cadena
+- edad : entero (Rango entre 18 y 45 años)
+- casado: boolean
+- salario: tipo numérico doble
+Métodos:
+- Constructor con y sin parámetros de entrada
+- Método que permita mostrar la clasificación según la edad
+de acuerdo al siguiente algoritmo:
+Si edad es menor o igual a 21, Principiante
+Si edad es >=22 y <=35, Intermedio
+Si edad es >35, Senior.-
+- Un método que permita aumentar el salario en un porcentaje que
+sería pasado como parámetro al método.
+2. Programador (Especialización de Empleado). Clase que hereda
+de Empleado todos los atributos y métodos.
+Atributos:
+- lineasDeCodigoPorHora : tipo entero
+- lenguajeDominante: tipo cadena
+Métodos:
+- Constructor con y sin parámetros de entrada.
+Para este programa, realizar un menú que permita introducir la
+opción de datos a ambas instancias de clase
+
+
  */
 
 package tp11_ej04;
