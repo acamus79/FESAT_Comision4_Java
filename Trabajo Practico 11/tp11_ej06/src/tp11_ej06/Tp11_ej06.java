c81deec9a66b60056f35026b627af88edfc9cbef
acamus79 <adriancamus1979@gmail.com>
2021-08-11 19:03:43 -0300

    Ejercicio 06

--- Trabajo Practico 11/tp11_ej06/src/tp11_ej06/Tp11_ej06.java
+++ Trabajo Practico 11/tp11_ej06/src/tp11_ej06/Tp11_ej06.java
@@ -2,6 +2,15 @@
  * Licencia Libre.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Realizar la codificación del siguiente diagrama de clases
+con  algún método que compartan entre las mismas
+
+
+              FIGURA
+
+CIRCULO     RECTANGULO      TRIANGULO
+
  */
 
 package tp11_ej06;
---
+++ Trabajo Practico 11/tp11_ej06/src/tp11_ej06/entidades/Figura.java
@@ -0,0 +1,18 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package tp11_ej06.entidades;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Figura {
+    
+    protected double area;
+    protected double perimetro;
+    
+    
+}
